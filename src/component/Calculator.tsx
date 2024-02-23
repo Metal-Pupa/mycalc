4ee97f1199035af4450b07ca58dd022cd1bedd1d
Angching <kosukeokino555@gmail.com>
2024-02-23 20:25:46 +0900

    製作中3

--- src/component/Calculator.tsx
+++ src/component/Calculator.tsx
@@ -2,10 +2,13 @@ import ButtonPanel from "./ButtonPanel";
 import Display from "./Display";
 
 export default function Calculator() {
+  const buttonHandler = (code: string) => {
+    console.log(code);
+  };
   return (
     <div>
       <Display />
-      <ButtonPanel />
+      <ButtonPanel buttonHandler={buttonHandler} />
     </div>
   );
 }
